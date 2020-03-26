2b5ce80038b11d57b852cd194578ee53d8db0889
Florian Reinhold <florian.reinhold@conplement.de>
2020-03-26 22:58:21 +0100

    #4387: simplyfied scripts and dobi scripts

--- gitversion/entrypoint.sh
+++ gitversion/entrypoint.sh
@@ -2,54 +2,54 @@
 set -o errexit   # abort on nonzero exitstatus
 set -o pipefail  # don't hide errors within pipes
 
-if [ -z "${GV_GIT}" ]; then
-  echo "No git folder supplied. Using default source '/repo'."
-  GV_GIT=/repo
+GIT=/git
+GEN=/gen
+
+# check if /git is a git repository
+if [ ! -d "/git" ]; then
+  echo "No git repository supplied. use docker -v PATH_TO_YOUR_REPO:/git"
+  exit 1
 fi
 
-if [ -z "${GV_GEN}" ]; then
-  echo "No generation folder supplied. Using default destination '/gen'."
-  GV_GEN=/gen
-fi
-
-if [ -z "${GV_USERID}" ]; then
+if [ -z "${USERID}" ]; then
   echo "No user id supplied. Using default id '9001'"
-  GV_USERID=9001
+  USERID=9001
 fi
 
-useradd --shell /bin/bash -u ${GV_USERID} -o -c "" -m user
+useradd --shell /bin/bash -u ${USERID} -o -c "" -m user
 
 ###### generate json ######
 
 # generate json file with git version infos
-GV_JSON_DIR=${GV_GEN}/json
-mkdir -p ${GV_JSON_DIR}
-dotnet /app/GitVersion.dll gittools/gitversion:5.1.4-linux-ubuntu-18.04-netcoreapp3.1 ${GV_GIT} > ${GV_JSON_DIR}/gitversion.json
+JSON_DIR=${GEN}/json
+mkdir -p ${JSON_DIR}
+dotnet /app/GitVersion.dll gittools/gitversion:5.1.4-linux-ubuntu-18.04-netcoreapp3.1 ${GIT} > ${JSON_DIR}/gitversion.json
 
 ###### generate env file ######
 
 # create or clear file
-GV_ENV_DIR=${GV_GEN}/env
-mkdir -p ${GV_ENV_DIR}
-> ${GV_ENV_DIR}/gitversion.env
+ENV_DIR=${GEN}/env
+mkdir -p ${ENV_DIR}
+> ${ENV_DIR}/gitversion.env
 
 
 # parse json and create sourceable file with git version infos
-for s in $(cat ${GV_JSON_DIR}/gitversion.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
-  echo "export GitVersion_$s" >> $GV_ENV_DIR/gitversion.env
+for s in $(cat ${JSON_DIR}/gitversion.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
+  echo "export GitVersion_$s" >> $ENV_DIR/gitversion.env
 done
 
 ###### generate plain files ######
 
 # create folder
-GV_PLAIN_DIR=${GV_GEN}/plain
-mkdir -p ${GV_PLAIN_DIR}
+PLAIN_DIR=${GEN}/plain
+mkdir -p ${PLAIN_DIR}
 
-for s in $(cat ${GV_JSON_DIR}/gitversion.json | jq -r keys[] ); do
-  > ${GV_PLAIN_DIR}/${s}
-  echo $(cat ${GV_JSON_DIR}/gitversion.json | jq -r .${s} ) >> ${GV_PLAIN_DIR}/${s}
+for s in $(cat ${JSON_DIR}/gitversion.json | jq -r keys[] ); do
+  > ${PLAIN_DIR}/${s}
+  echo $(cat ${JSON_DIR}/gitversion.json | jq -r .${s} ) >> ${PLAIN_DIR}/${s}
 done
 
 # change ownership to local user
-chown -R user:user ${GV_GEN}
+chown -R user:user ${GEN}
 
+echo "Generation complete."
