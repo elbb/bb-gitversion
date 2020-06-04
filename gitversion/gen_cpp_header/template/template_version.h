4a0540aca02239b6ce7ae80c28020cd0ddef2ae8
Jörg Zeidler <joerg.zeidler@conplement.de>
2020-06-04 13:17:51 +0200

    #18: added prefix for  version defines; added #ifndef preprocessor directive

--- gitversion/gen_cpp_header/template/template_version.h
+++ gitversion/gen_cpp_header/template/template_version.h
@@ -1,8 +1,13 @@
-#define MAJOR                 ({{ver.major -}})
-#define MINOR                 {{ver.minor -}}
-#define PATCH                 {{ver.patch -}}
-#define SEMVER                {{ver.semver -}}
-#define FULLSEMVER            {{ver.fullsemver -}}
-#define INFORMATIONALVERSION  {{ver.informationalversion -}}
-#define BRANCHNAME            {{ver.branchname -}}
-#define SHORTSHA              {{ver.shortsha -}}
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define VERSION_MAJOR                 ({{ver.major -}})
+#define VERSION_MINOR                 ({{ver.minor -}})
+#define VERSION_PATCH                 ({{ver.patch -}})
+#define VERSION_SEMVER                ({{ver.semver -}})
+#define VERSION_FULLSEMVER            ({{ver.fullsemver -}})
+#define VERSION_INFORMATIONALVERSION  ({{ver.informationalversion -}})
+#define VERSION_BRANCHNAME            ({{ver.branchname -}})
+#define VERSION_SHORTSHA              ({{ver.shortsha -}})
+
+#endif
