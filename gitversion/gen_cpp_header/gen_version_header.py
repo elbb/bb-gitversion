ce4d566d0dfc68bbcf0102035d2608205af1bc5f
Jörg Zeidler <joerg.zeidler@conplement.de>
2020-06-09 10:49:56 +0200

    #18: generate all gitversion information into the cpp header file;

--- gitversion/gen_cpp_header/gen_version_header.py
+++ gitversion/gen_cpp_header/gen_version_header.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python2
 
 from jinja2 import Environment, FileSystemLoader
-import sh
+from collections import OrderedDict 
+import json
 
-file_loader = FileSystemLoader('/tools/gen_cpp_header/template')
-env = Environment(loader=file_loader)
-env.trim_blocks = False
-env.lstrip_blocks = False
+file_loader = FileSystemLoader('template')
+env = Environment(trim_blocks=True, lstrip_blocks=True, loader=file_loader) 
 
 template = env.get_template('template_version.h')
 
-version = {}
-version['major'] = sh.jq( "-r",".Major|tostring","/gen/json/gitversion.json")
-version['minor'] = sh.jq( "-r",".Minor|tostring","/gen/json/gitversion.json")
-version['patch'] = sh.jq( "-r",".Patch|tostring","/gen/json/gitversion.json")
-version['semver'] = sh.jq( "-r",".SemVer","/gen/json/gitversion.json")
-version['fullsemver'] = sh.jq( "-r",".FullSemVer","/gen/json/gitversion.json")
-version['informationalversion'] = sh.jq( "-r",".InformationalVersion","/gen/json/gitversion.json")
-version['branchname'] = sh.jq( "-r",".BranchName","/gen/json/gitversion.json")
-version['shortsha'] = sh.jq( "-r",".ShortSha","/gen/json/gitversion.json")
+# read file
+with open('/gen/json/gitversion.json', 'r') as file:
+    data=file.read()
 
-template.stream(ver=version).dump("/gen/cpp/version.h")
+# Closing file 
+file.close() 
+
+# returns JSON object as a dictionary 
+dict = json.loads(data) 
+
+# if you would like to delete empty version elements, use this peace of code
+# empty_keys = [k for k,v in dict.iteritems() if len(str(v)) is 0]
+# for k in empty_keys:
+#     del dict[k]
+
+# sort dict by key names
+dict_sort = OrderedDict(sorted(dict.items()))
+
+# generate header file 
+template.stream(ver=dict_sort).dump("/gen/cpp/version.h")
--- gitversion/gen_cpp_header/template/template_version.h
+++ gitversion/gen_cpp_header/template/template_version.h
@@ -1,13 +1,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION_MAJOR                 ({{ver.major -}})
-#define VERSION_MINOR                 ({{ver.minor -}})
-#define VERSION_PATCH                 ({{ver.patch -}})
-#define VERSION_SEMVER                ({{ver.semver -}})
-#define VERSION_FULLSEMVER            ({{ver.fullsemver -}})
-#define VERSION_INFORMATIONALVERSION  ({{ver.informationalversion -}})
-#define VERSION_BRANCHNAME            ({{ver.branchname -}})
-#define VERSION_SHORTSHA              ({{ver.shortsha -}})
-
+#include <string>
+namespace version { 
+{% for key, value in ver.items() %}
+constexpr std::string  {{key}} ("{{value}}");
+{% endfor %}
+}
 #endif
